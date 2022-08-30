ca656cfde02da9cda4ccf8af246429becee582a6
MartiBianchi <bianchimartiniano@gmail.com>
2022-08-30 11:28:45 -0300

    proyecto finalizado

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -4,167 +4,191 @@ import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
 import { Alerta } from './Alerta';
+import { Spinner } from './Spinner';
 
-export const Formulario = () => {
-	const navigate = useNavigate();
+export const Formulario = ({ cliente, cargando }) => {
+  const navigate = useNavigate();
 
-	// Validaciones
-	const nuevoClienteSchema = Yup.object().shape({
-		nombre: Yup.string()
-			.min(3, 'El nombre es muy corto')
-			.max(20, 'El nombre es muy largo')
-			.required('El nombre del cliente es obligatorio'),
-		empresa: Yup.string().required('El nombre de la empresa es obligatorio'),
-		email: Yup.string()
-			.email('El email no es válido')
-			.required('El email es obligatorio'),
-		telefono: Yup.number()
-			.positive('Número no válido')
-			.integer('Número no válido')
-			.typeError('Número no válido')
-	});
+  // Validaciones
+  const nuevoClienteSchema = Yup.object().shape({
+    nombre: Yup.string()
+      .min(3, 'El nombre es muy corto')
+      .max(20, 'El nombre es muy largo')
+      .required('El nombre del cliente es obligatorio'),
+    empresa: Yup.string().required('El nombre de la empresa es obligatorio'),
+    email: Yup.string()
+      .email('El email no es válido')
+      .required('El email es obligatorio'),
+    telefono: Yup.number()
+      .positive('Número no válido')
+      .integer('Número no válido')
+      .typeError('Número no válido'),
+  });
 
-	const handleSubmit = async (values) => {
-		try {
-			const url = 'http://localhost:4000/clientes';
+  const handleSubmit = async (values) => {
+    try {
+      let respuesta;
 
-			const respuesta = await fetch(url, {
-				method: 'POST',
-				body: JSON.stringify(values),
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			});
+      if (cliente.id) {
+        // Editar registro
+        const url = `http://localhost:3000/clientes/${cliente.id}`;
 
-			const resultado = await respuesta.json();
+        respuesta = await fetch(url, {
+          method: 'PUT',
+          body: JSON.stringify(values),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+      } else {
+        // Nuevo registro
+        const url = 'http://localhost:3000/clientes';
 
-			console.log(resultado);
+        respuesta = await fetch(url, {
+          method: 'POST',
+          body: JSON.stringify(values),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+      }
 
-			navigate('/clientes');
-		} catch (error) {
-			console.log(error);
-		}
-	};
+      await respuesta.json();
 
-	return (
-		<div className='bg-white mt-10 px-5 py-10 rounder-md shadow-md md:w-3/4 mx-auto'>
-			<h1 className='text-gray-600 font-bold text-xl uppercase text-center'>
-				Agregar Cliente
-			</h1>
+      navigate('/clientes');
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-			<Formik
-				initialValues={{
-					nombre: '',
-					empresa: '',
-					email: '',
-					telefono: '',
-					notas: ''
-				}}
-				onSubmit={async (values, { resetForm }) => {
-					await handleSubmit(values);
+  return cargando ? (
+    <Spinner />
+  ) : (
+    <div className='bg-white mt-10 px-5 py-10 rounder-md shadow-md md:w-3/4 mx-auto'>
+      <h1 className='text-gray-600 font-bold text-xl uppercase text-center'>
+        {cliente?.nombre ? 'Editar Cliente' : 'Agregar Cliente'}
+      </h1>
 
-					resetForm();
-				}}
-				validationSchema={nuevoClienteSchema}
-			>
-				{({ errors, touched }) => {
-					return (
-						<Form className='mt-10'>
-							<div className='mb-4'>
-								<label className='text-gray-800' htmlFor='nombre'>
-									Nombre:
-								</label>
-								<Field
-									id='nombre'
-									type='text'
-									className='mt-2 block w-full p-3 bg-gray-100 capitalize'
-									placeholder='Nombre del Cliente'
-									name='nombre'
-								/>
+      <Formik
+        initialValues={{
+          nombre: cliente?.nombre ?? '',
+          empresa: cliente?.empresa ?? '',
+          email: cliente?.email ?? '',
+          telefono: cliente?.telefono ?? '',
+          notas: cliente?.notas ?? '',
+        }}
+        enableReinitialize={true}
+        onSubmit={async (values, { resetForm }) => {
+          await handleSubmit(values);
 
-								{/* Validación en tiempo real */}
-								{errors.nombre && touched.nombre ? (
-									<Alerta>{errors.nombre}</Alerta>
-								) : null}
-							</div>
+          resetForm();
+        }}
+        validationSchema={nuevoClienteSchema}
+      >
+        {({ errors, touched }) => {
+          return (
+            <Form className='mt-10'>
+              <div className='mb-4'>
+                <label className='text-gray-800' htmlFor='nombre'>
+                  Nombre:
+                </label>
+                <Field
+                  id='nombre'
+                  type='text'
+                  className='mt-2 block w-full p-3 bg-gray-100 capitalize'
+                  placeholder='Nombre del Cliente'
+                  name='nombre'
+                />
 
-							<div className='mb-4'>
-								<label className='text-gray-800' htmlFor='empresa'>
-									Empresa:
-								</label>
-								<Field
-									id='empresa'
-									type='text'
-									className='mt-2 block w-full p-3 bg-gray-100 capitalize'
-									placeholder='Empresa del Cliente'
-									name='empresa'
-								/>
+                {/* Validación en tiempo real */}
+                {errors.nombre && touched.nombre ? (
+                  <Alerta>{errors.nombre}</Alerta>
+                ) : null}
+              </div>
 
-								{/* Validación en tiempo real */}
-								{errors.empresa && touched.empresa ? (
-									<Alerta>{errors.empresa}</Alerta>
-								) : null}
-							</div>
+              <div className='mb-4'>
+                <label className='text-gray-800' htmlFor='empresa'>
+                  Empresa:
+                </label>
+                <Field
+                  id='empresa'
+                  type='text'
+                  className='mt-2 block w-full p-3 bg-gray-100 capitalize'
+                  placeholder='Empresa del Cliente'
+                  name='empresa'
+                />
 
-							<div className='mb-4'>
-								<label className='text-gray-800' htmlFor='email'>
-									E-mail:
-								</label>
-								<Field
-									id='email'
-									type='email'
-									className='mt-2 block w-full p-3 bg-gray-100'
-									placeholder='Email del Cliente'
-									name='email'
-								/>
+                {/* Validación en tiempo real */}
+                {errors.empresa && touched.empresa ? (
+                  <Alerta>{errors.empresa}</Alerta>
+                ) : null}
+              </div>
 
-								{/* Validación en tiempo real */}
-								{errors.email && touched.email ? (
-									<Alerta>{errors.email}</Alerta>
-								) : null}
-							</div>
+              <div className='mb-4'>
+                <label className='text-gray-800' htmlFor='email'>
+                  E-mail:
+                </label>
+                <Field
+                  id='email'
+                  type='email'
+                  className='mt-2 block w-full p-3 bg-gray-100'
+                  placeholder='Email del Cliente'
+                  name='email'
+                />
 
-							<div className='mb-4'>
-								<label className='text-gray-800' htmlFor='telefono'>
-									Teléfono:
-								</label>
-								<Field
-									id='telefono'
-									type='tel'
-									className='mt-2 block w-full p-3 bg-gray-100'
-									placeholder='Teléfono del Cliente'
-									name='telefono'
-								/>
+                {/* Validación en tiempo real */}
+                {errors.email && touched.email ? (
+                  <Alerta>{errors.email}</Alerta>
+                ) : null}
+              </div>
 
-								{/* Validación en tiempo real */}
-								{errors.telefono && touched.telefono ? (
-									<Alerta>{errors.telefono}</Alerta>
-								) : null}
-							</div>
+              <div className='mb-4'>
+                <label className='text-gray-800' htmlFor='telefono'>
+                  Teléfono:
+                </label>
+                <Field
+                  id='telefono'
+                  type='tel'
+                  className='mt-2 block w-full p-3 bg-gray-100'
+                  placeholder='Teléfono del Cliente'
+                  name='telefono'
+                />
 
-							<div className='mb-4'>
-								<label className='text-gray-800' htmlFor='notas'>
-									Notas:
-								</label>
-								<Field
-									as='textarea'
-									id='notas'
-									type='text'
-									className='mt-2 block w-full p-3 bg-gray-100'
-									placeholder='Notas del Cliente'
-									name='notas'
-								/>
-							</div>
+                {/* Validación en tiempo real */}
+                {errors.telefono && touched.telefono ? (
+                  <Alerta>{errors.telefono}</Alerta>
+                ) : null}
+              </div>
 
-							<input
-								type='submit'
-								value='Agregar Cliente'
-								className='mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold text-lg cursor-pointer hover:bg-blue-700'
-							/>
-						</Form>
-					);
-				}}
-			</Formik>
-		</div>
-	);
+              <div className='mb-4'>
+                <label className='text-gray-800' htmlFor='notas'>
+                  Notas:
+                </label>
+                <Field
+                  as='textarea'
+                  id='notas'
+                  type='text'
+                  className='mt-2 block w-full p-3 bg-gray-100'
+                  placeholder='Notas del Cliente'
+                  name='notas'
+                />
+              </div>
+
+              <input
+                type='submit'
+                value={cliente?.nombre ? 'Editar Cliente' : 'Agregar Cliente'}
+                className='mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold text-lg cursor-pointer hover:bg-blue-700'
+              />
+            </Form>
+          );
+        }}
+      </Formik>
+    </div>
+  );
+};
+
+// Default Props
+Formulario.defaultProps = {
+  cliente: {},
+  cargando: false,
 };
--- src/pages/VerCliente.jsx
+++ src/pages/VerCliente.jsx
@@ -1,81 +1,82 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import { Spinner } from '../components/Spinner';
+
 export const VerCliente = () => {
-	const [cliente, setCliente] = useState({});
-	const [cargando, setCargando] = useState(false);
+  const [cliente, setCliente] = useState({});
+  const [cargando, setCargando] = useState(true);
 
-	const { id } = useParams();
+  const { id } = useParams();
 
-	useEffect(() => {
-		setCargando(!cargando);
-		const obtenerClienteAPI = async () => {
-			try {
-				const url = `http://localhost:4000/clientes/${id}`;
+  useEffect(() => {
+    const obtenerClienteAPI = async () => {
+      try {
+        const url = `http://localhost:3000/clientes/${id}`;
 
-				const respuesta = await fetch(url);
+        const respuesta = await fetch(url);
 
-				const resultado = await respuesta.json();
+        const resultado = await respuesta.json();
 
-				setCliente(resultado);
-			} catch (error) {
-				console.log(error);
-			}
-			setCargando(false);
-		};
+        setCliente(resultado);
+      } catch (error) {
+        console.log(error);
+      }
+      setCargando(false);
+    };
 
-		obtenerClienteAPI();
-	}, []);
+    obtenerClienteAPI();
+  }, []);
 
-	return Object.keys(cliente).length === 0 ? (
-		<p>No hay Resultados</p>
-	) : (
-		<>
-			{cargando ? (
-				'cargando...'
-			) : (
-				<>
-					<h1 className='font-black text-4xl text-blue-900'>Ver Cliente</h1>
-					<p className='mt-3'>Información del Cliente</p>
+  return Object.keys(cliente).length === 0 ? (
+    <p>No hay Resultados</p>
+  ) : (
+    <>
+      {cargando ? (
+        <Spinner />
+      ) : (
+        <>
+          <h1 className='font-black text-4xl text-blue-900'>Ver Cliente</h1>
+          <p className='mt-3'>Información del Cliente</p>
 
-					{cliente.nombre && (
-						<p className='text-xl mt-10 text-gray-600'>
-							<span className='text-gray-800 uppercase font-bold'>
-								Cliente:{' '}
-							</span>
-							{cliente.nombre}
-						</p>
-					)}
-					{cliente.email && (
-						<p className='text-xl mt-4 text-gray-600'>
-							<span className='text-gray-800 uppercase font-bold'>Email: </span>
-							{cliente.email}
-						</p>
-					)}
-					{cliente.telefono && (
-						<p className='text-xl mt-4 text-gray-600'>
-							<span className='text-gray-800 uppercase font-bold'>
-								Teléfono:{' '}
-							</span>
-							{cliente.telefono}
-						</p>
-					)}
-					{cliente.empresa && (
-						<p className='text-xl mt-4 text-gray-600'>
-							<span className='text-gray-800 uppercase font-bold'>
-								Empresa:{' '}
-							</span>
-							{cliente.empresa}
-						</p>
-					)}
-					{cliente.notas && (
-						<p className='text-xl mt-4 text-gray-600'>
-							<span className='text-gray-800 uppercase font-bold'>Notas: </span>
-							{cliente.notas}
-						</p>
-					)}
-				</>
-			)}
-		</>
-	);
+          {cliente.nombre && (
+            <p className='text-xl mt-10 text-gray-600'>
+              <span className='text-gray-800 uppercase font-bold'>
+                Cliente:{' '}
+              </span>
+              {cliente.nombre}
+            </p>
+          )}
+          {cliente.email && (
+            <p className='text-xl mt-4 text-gray-600'>
+              <span className='text-gray-800 uppercase font-bold'>Email: </span>
+              {cliente.email}
+            </p>
+          )}
+          {cliente.telefono && (
+            <p className='text-xl mt-4 text-gray-600'>
+              <span className='text-gray-800 uppercase font-bold'>
+                Teléfono:{' '}
+              </span>
+              {cliente.telefono}
+            </p>
+          )}
+          {cliente.empresa && (
+            <p className='text-xl mt-4 text-gray-600'>
+              <span className='text-gray-800 uppercase font-bold'>
+                Empresa:{' '}
+              </span>
+              {cliente.empresa}
+            </p>
+          )}
+          {cliente.notas && (
+            <p className='text-xl mt-4 text-gray-600'>
+              <span className='text-gray-800 uppercase font-bold'>Notas: </span>
+              {cliente.notas}
+            </p>
+          )}
+        </>
+      )}
+    </>
+  );
 };
